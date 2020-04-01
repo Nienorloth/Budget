3d9be351de6c23ab038f555d9c849c3bb35feb43
Mariana Rascon <51216146+Nienorloth@users.noreply.github.com>
2020-04-01 17:07:03 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Question from './components/Question';
+import Formulario from './components/Formulario';
+
 
 function App() {
+
+  //definir el state
+  const [ presupuesto, guardarPresupuesto ] = useState(0);
+  const [ restante, guardarRestante ] = useState(0);
+  const [ mostrarpregunta, actualizarPregunta ] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>Gasto semanal</h1>
+          <div className="contenido-principal contenido">
+            { mostrarpregunta ? (
+            <Question
+            guardarPresupuesto={guardarPresupuesto}
+            guardarRestante={guardarRestante}
+            actualizarPregunta={actualizarPregunta}
+            />
+            )  : 
+            (
+             <div className="row">
+                <div className="one-half column">
+                  <Formulario />
+                </div>
+                <div className="one-half column">
+                  2
+                </div>
+
+              </div>
+            ) 
+          }
+            
+            
+          </div>
       </header>
     </div>
   );
